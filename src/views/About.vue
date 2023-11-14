d010232b4d6079c9bab47ae9ea452d064e68a92c
Gismat A. Mammadov <gismat.mammadov@smb.gov.lan>
2023-11-14 17:58:12 +0400

    Navigation Rendering

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1>This is About us page</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "AboutView.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+template>
+<template>
+  <main>
+    <h1>This is Home page</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<style scoped>
+
+</style>
